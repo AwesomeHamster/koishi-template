bc8c39738f8f9f6b26efd4fd82dea4d75593aadf
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-09-12 07:10:59 +0000

    feat: project skeleton to initialise koishi plugin

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Context, Schema } from 'koishi'
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({}) 
+
+export const name = 'koishi-template'
+
+export async function apply(ctx: Context): Promise<void> {
+  // ...
+}
